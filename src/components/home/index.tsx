18c3d3f504ec37a0daa2dac7689f1f2ad0281fe6
João Manoel <joaom.developer@gmail.com>
2022-10-27 19:56:51 -0300

    Imagem do background atualizada

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,33 +3,13 @@ import homeBackground2 from '../../assets/imgs/Background-Home-2.jpg'
 import homeBackground3 from '../../assets/imgs/Background-Home-3.jpg'
 import { useEffect, useState } from 'react';
 
-const Home = () => { 
-  const [ backgroundt, setBackground ] = useState( homeBackground )
-
-  const filterUnitsOfMeasure = () => {
-    if ( window.innerWidth <= 804 ) setBackground(homeBackground3)
-    else if ( window.innerWidth <= 1439 ) setBackground(homeBackground2)
-    else if ( window.innerWidth > 1439 ) setBackground(homeBackground)
-  }
-
-  useEffect( () => {
-
-    window.addEventListener('load', filterUnitsOfMeasure )
-    window.addEventListener('resize', filterUnitsOfMeasure )
-  }, [])  
-
-  // useEffect(() => {
-  //   window
-  //   .matchMedia("(max-width: 768px)")
-  //   .
-  // }, []);
-
+const Home = () => {
   return (
-    <section id='home' style={{ background: `url(${ backgroundt })`}}>
+    <section id='home' style={{ backgroundSize: 'cover', background: `url(${ homeBackground })`}}>
       <div id='home__info-container'>
         <div id='home__info'>
           <h1 id='home__title'>Welcome to WorldTrip, your international travel website</h1>
-          <p id='home__text'>Only on worldTrip can you get a trip with a paid hotel and coffee included</p>
+          <p id='home__text'>Only at World Trip can you get a trip with a paid hotel and coffee included, we have more than 10 years of work and about 100,000 trips.</p>
           <button id='home__button'>Contact</button>
         </div>
       </div>
